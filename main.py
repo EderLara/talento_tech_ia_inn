6fddc60487d6dced30973d12714ff2e276486216
Janettgm2015-ux <geromvrebolledo@gmail.com>
2026-02-10 21:22:48 -0500

    Santiago commit2

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print('Hola Mundo, talento Tech')
-<<<<<<< HEAD
 print('Santiago G - Analítica Colmédicos')
-=======
 print('Eder Lara - Profesor')
 print('Juan José B - Estudiante')
->>>>>>> 85f2555703a5f772d630e4f1114888d65d2de616
